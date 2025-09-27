828264a0985ca36d5243d2e6633c4796c34f1a26
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-09-27 08:25:56 +0530

    largest number

--- Dsa/Array/1d array/_1_largestNumber.java
+++ Dsa/Array/1d array/_1_largestNumber.java
@@ -1,3 +1,21 @@
-public class _1_ largestNumber{
-  void main
+import java.util.Arrays;
+
+public class _1_largestNumber {
+  public static void main(String[] args) {
+    int arr[] = { 3, 2, 1, 5, 2 };
+    System.out.println(Arrays.toString(arr));
+
+    System.out.println(" largest element  is: " + largestNumber(arr));
+  }
+
+  public static int largestNumber(int arr[]) {
+    int largest = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+
+    }
+    return largest;
+  }
 }
